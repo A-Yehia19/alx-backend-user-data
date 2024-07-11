cce6e2c915491ff545b6c1ed308e67e785e9958d
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-07-11 13:56:03 +0300

    improvea paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,10 +13,18 @@ class Auth:
 
         if path[-1] != '/':
             path += '/'
+        if excluded_paths[-1] != '/':
+            excluded_paths += '/'
+
+        astericks = [stars[:-1]
+                     for stars in excluded_paths if stars[-1] == '*']
+
+        for stars in astericks:
+            if path.startswith(stars):
+                return False
 
         if path in excluded_paths:
             return False
-
         return True
 
     def authorization_header(self, request=None) -> str:
