0718f3dd6ecda8a89d16095e27a8c3ddfe219061
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-07-11 13:01:00 +0300

    added permissions

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,7 @@ class Auth:
     """ Auth class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         return None
 
