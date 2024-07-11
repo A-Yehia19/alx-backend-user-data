844b58bfc77036d1776f5add85af6c4b71a11213
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-07-11 13:07:49 +0300

    validate endpoints

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,16 @@ class Auth:
     """ Class to manage the API authentication """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Method for validating if endpoint requires auth """
-        return False
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        
+        if path[-1] != '/':
+            path += '/'
+        
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Method that handles authorization header """
